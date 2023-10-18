31e728d791f2df4f99219a53ce48e9d5ae16503b
drishya-nair <nair.drish@gmail.com>
2023-10-18 14:58:57 -0700

    finished cats study demographics page

---
+++ src/study-cats/webpack.config.js
@@ -0,0 +1,47 @@
+var path = require("path");
+
+var config = {
+  entry: path.join(__dirname, "study.js"),
+  output: {
+    path: path.join(__dirname, "js"),
+    filename: "bundle-model.min.js"
+  },
+  module: {
+    rules: [
+        {
+          test: require.resolve('jquery'),
+            use: [{
+              loader: 'expose-loader',
+              options: {
+                exposes: "jquery",
+              },
+            },
+            {
+              loader: 'expose-loader',
+              options: {
+                exposes: '$',
+              },
+            }
+            ]
+        },
+        {
+          test: /.*\.html$/, loader: "handlebars-loader"
+        }
+    ]
+  },
+  externals: [
+    /^(jquery.i18n|\$)$/i,
+    {
+       d3: "d3"
+    }
+  ],
+  resolve: {
+    fallback: {
+      "fs": false,
+      "path": false,
+      "url": false
+    },
+  }
+};
+
+module.exports = config;
